d5d0dd8b8313206254d571d59857d3a138dab8df
Joshua Ruppe <me@joshruppe.com>
2026-06-16 11:50:54 -0400

    updated to remove deprecated commands and add new ones for Windows privilege escalation.

--- winprivesc.bat
+++ winprivesc.bat
@@ -13,7 +13,7 @@ echo "| |/ |/ / / / / / ____/ /  / /| |/ / /___(__  ) /__
 echo "|__/|__/_/_/ /_/_/   /_/  /_/ |___/_____/____/\___/
 echo.
 echo Windows Enumeration and Privilege Escalation Script
-echo www.joshruppe.com ^| Twitter: @josh_ruppe
+echo www.joshruppe.com ^| Bluesky: @joshruppe.com
 echo.
 
 echo 1 - All to Report
@@ -22,29 +22,31 @@ echo 3 - Storage
 echo 4 - Networking
 echo 5 - Processes
 echo 6 - User Info
-echo 7 - Exit
+echo 7 - Privilege Escalation
+echo 8 - Exit
 echo.
+SET "C="
 SET /P C=Select^>
 echo.
-IF %C%==1 GOTO ALL
-IF %C%==2 GOTO OS
-IF %C%==3 GOTO STORAGE
-IF %C%==4 GOTO NETWORK
-IF %C%==5 GOTO PROCESSES
-IF %C%==6 GOTO USERS
-IF %C%==7 GOTO EXIT
+IF "%C%"=="1" GOTO ALL
+IF "%C%"=="2" GOTO OS
+IF "%C%"=="3" GOTO STORAGE
+IF "%C%"=="4" GOTO NETWORK
+IF "%C%"=="5" GOTO PROCESSES
+IF "%C%"=="6" GOTO USERS
+IF "%C%"=="7" GOTO PRIVESC
+IF "%C%"=="8" GOTO EXIT
+echo Invalid selection, please choose 1-8.
+echo.
+GOTO MENU
 
 :ALL
 echo WinPrivEsc >> report.txt
 echo Windows Enumeration and Privilege Escalation Script>> report.txt
-echo www.joshruppe.com ^| Twitter: @josh_ruppe>> report.txt
+echo www.joshruppe.com ^| Bluesky: @joshruppe.com>> report.txt
 echo.>> report.txt
-echo Report generated: >> report.txt
+echo Report generated: %DATE% %TIME% >> report.txt
 echo. >> report.txt
-for /F "tokens=* USEBACKQ" %%F IN ('Date') do (
-set Date=%%F
-echo %Date% >> report.txt
-)
 echo __________________________ >> report.txt
 echo. >> report.txt
 echo      OPERATING SYSTEM >> report.txt
@@ -83,14 +85,15 @@ for /F "tokens=2" %%a IN ('findstr /v ".TXT" hotfix.txt') do (
   set Hot=%%~a
   echo !Hot!>> report.txt
 )
+endlocal
 echo.>> report.txt
 echo [++Hosts File]>> report.txt
 echo.>> report.txt
-more c:\WINDOWS\System32\drivers\etc\hosts>> report.txt
+more %SystemRoot%\System32\drivers\etc\hosts>> report.txt
 echo.>> report.txt
 echo [++Networks File]>> report.txt
 echo.>> report.txt
-more c:\WINDOWS\System32\drivers\etc\networks>> report.txt
+more %SystemRoot%\System32\drivers\etc\networks>> report.txt
 echo.>> report.txt
 echo [++Running Services]>> report.txt
 echo.>> report.txt
@@ -132,7 +135,7 @@ echo [++ARP]>> report.txt
 arp -a>> report.txt
 echo.>> report.txt
 echo [++Firewall Configuration]>> report.txt
-netsh firewall show config>> report.txt
+netsh advfirewall show allprofiles>> report.txt
 echo [++Domain]>> report.txt
 echo.>> report.txt
 set userdomain>> report.txt
@@ -164,6 +167,74 @@ net users>> report.txt
 echo.>> report.txt
 echo [++User Groups]>> report.txt
 net localgroup>> report.txt
+echo.>> report.txt
+echo.>> report.txt
+echo ____________________________>> report.txt
+echo.>> report.txt
+echo      PRIVILEGE ESCALATION >> report.txt
+echo ____________________________>> report.txt
+echo.>> report.txt
+echo [++User Privileges - look for SeImpersonate/SeAssignPrimaryToken/SeBackup/SeDebug]>> report.txt
+echo.>> report.txt
+whoami /priv>> report.txt
+echo.>> report.txt
+echo [++Token Groups and SIDs]>> report.txt
+echo.>> report.txt
+whoami /groups>> report.txt
+echo.>> report.txt
+echo [++AlwaysInstallElevated - HKLM (1 = exploitable)]>> report.txt
+echo.>> report.txt
+reg query HKLM\SOFTWARE\Policies\Microsoft\Windows\Installer /v AlwaysInstallElevated >> report.txt 2>nul
+echo.>> report.txt
+echo [++AlwaysInstallElevated - HKCU (1 = exploitable)]>> report.txt
+echo.>> report.txt
+reg query HKCU\SOFTWARE\Policies\Microsoft\Windows\Installer /v AlwaysInstallElevated >> report.txt 2>nul
+echo.>> report.txt
+echo [++Auto-start Services Outside Windows Dir - check for unquoted paths containing spaces]>> report.txt
+echo.>> report.txt
+wmic service get name,displayname,pathname,startmode 2>nul | findstr /i "Auto" | findstr /i /v "C:\Windows\\" >> report.txt
+echo.>> report.txt
+echo [++Stored Credentials - cmdkey]>> report.txt
+echo.>> report.txt
+cmdkey /list>> report.txt
+echo.>> report.txt
+echo [++Winlogon Autologon Credentials]>> report.txt
+echo.>> report.txt
+reg query "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" 2>nul | findstr /i "DefaultUserName DefaultDomainName DefaultPassword AutoAdminLogon">> report.txt
+echo.>> report.txt
+echo [++Scheduled Tasks (non-Microsoft) - look for SYSTEM tasks with writable binaries]>> report.txt
+echo.>> report.txt
+for /f "tokens=1 delims=," %%t in ('schtasks /query /fo csv /nh 2^>nul ^| findstr /i /v "\\Microsoft\\"') do schtasks /query /tn %%t /fo LIST /v >> report.txt 2>nul
+echo.>> report.txt
+echo [++Unquoted Service Path Candidates (wmic-free) - spaced path, unquoted, non-Windows]>> report.txt
+echo.>> report.txt
+setlocal enabledelayedexpansion
+for /f "tokens=1,2,*" %%a in ('reg query HKLM\SYSTEM\CurrentControlSet\Services /s /v ImagePath 2^>nul ^| findstr /i "ImagePath"') do (
+  set "ip=%%c"
+  set noq=!ip:"=!
+  if "!ip!"=="!noq!" (
+    echo !ip!| findstr /c:" " >nul && echo !ip!| findstr /i /v "Windows" >nul && echo !ip!>> report.txt
+  )
+)
+endlocal
+echo.>> report.txt
+echo [++Unattend / Sysprep Credential Files]>> report.txt
+echo.>> report.txt
+for %%f in ("%SystemRoot%\Panther\Unattend.xml" "%SystemRoot%\Panther\Unattend\Unattended.xml" "%SystemRoot%\Panther\Unattended.xml" "%SystemRoot%\System32\Sysprep\sysprep.xml" "%SystemRoot%\System32\Sysprep\sysprep.inf" "%SystemDrive%\unattend.xml") do if exist %%f (
+  echo --- %%f --->> report.txt
+  type %%f 2>nul | findstr /i "password username useraccount autologon">> report.txt
+)
+echo.>> report.txt
+echo [++Writable Directories in PATH (DLL hijack surface)]>> report.txt
+echo.>> report.txt
+setlocal enabledelayedexpansion
+for %%d in ("%PATH:;=" "%") do (
+  set "dir=%%~d"
+  if exist "!dir!\" (
+    icacls "!dir!" 2>nul | findstr /i "Users:( Everyone:( \Users:(" | findstr /i "(F) (M) (W)" >nul && echo POTENTIALLY WRITABLE: !dir!>> report.txt
+  )
+)
+endlocal
 echo.>> report.txt
 echo Done, check report.txt
 echo.
@@ -209,14 +280,15 @@ for /F "tokens=2" %%a IN ('findstr /v ".TXT" hotfix.txt') do (
   set Hot=%%~a
   echo !Hot!
 )
+endlocal
 echo.
 echo [++Hosts File]
 echo.
-more c:\WINDOWS\System32\drivers\etc\hosts
+more %SystemRoot%\System32\drivers\etc\hosts
 echo.
 echo [++Networks File]
 echo.
-more c:\WINDOWS\System32\drivers\etc\networks
+more %SystemRoot%\System32\drivers\etc\networks
 echo.
 echo [++Running Services]
 echo.
@@ -265,7 +337,7 @@ echo [++ARP]
 arp -a
 echo.
 echo [++Firewall Configuration]
-netsh firewall show config
+netsh advfirewall show allprofiles
 echo [++Domain]
 echo.
 set userdomain
@@ -284,7 +356,7 @@ echo [++Tasklist]
 tasklist /v
 echo.
 echo [++Drivers Installed]
-driverquery /vw
+driverquery /v
 del systeminfo.txt
 del hotfix.txt
 EXIT /B
@@ -304,6 +376,78 @@ net users
 echo.
 echo [++User Groups]
 net localgroup
+echo.
+del systeminfo.txt
+del hotfix.txt
+EXIT /B
+
+:PRIVESC
+echo ____________________________
+echo.
+echo      PRIVILEGE ESCALATION
+echo ____________________________
+echo.
+echo [++User Privileges - look for SeImpersonate/SeAssignPrimaryToken/SeBackup/SeDebug]
+echo.
+whoami /priv
+echo.
+echo [++Token Groups and SIDs]
+echo.
+whoami /groups
+echo.
+echo [++AlwaysInstallElevated - HKLM (1 = exploitable)]
+echo.
+reg query HKLM\SOFTWARE\Policies\Microsoft\Windows\Installer /v AlwaysInstallElevated 2>nul
+echo.
+echo [++AlwaysInstallElevated - HKCU (1 = exploitable)]
+echo.
+reg query HKCU\SOFTWARE\Policies\Microsoft\Windows\Installer /v AlwaysInstallElevated 2>nul
+echo.
+echo [++Auto-start Services Outside Windows Dir - check for unquoted paths containing spaces]
+echo.
+wmic service get name,displayname,pathname,startmode 2>nul | findstr /i "Auto" | findstr /i /v "C:\Windows\\"
+echo.
+echo [++Stored Credentials - cmdkey]
+echo.
+cmdkey /list
+echo.
+echo [++Winlogon Autologon Credentials]
+echo.
+reg query "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" 2>nul | findstr /i "DefaultUserName DefaultDomainName DefaultPassword AutoAdminLogon"
+echo.
+echo [++Scheduled Tasks (non-Microsoft) - look for SYSTEM tasks with writable binaries]
+echo.
+for /f "tokens=1 delims=," %%t in ('schtasks /query /fo csv /nh 2^>nul ^| findstr /i /v "\\Microsoft\\"') do schtasks /query /tn %%t /fo LIST /v 2>nul
+echo.
+echo [++Unquoted Service Path Candidates (wmic-free) - spaced path, unquoted, non-Windows]
+echo.
+setlocal enabledelayedexpansion
+for /f "tokens=1,2,*" %%a in ('reg query HKLM\SYSTEM\CurrentControlSet\Services /s /v ImagePath 2^>nul ^| findstr /i "ImagePath"') do (
+  set "ip=%%c"
+  set noq=!ip:"=!
+  if "!ip!"=="!noq!" (
+    echo !ip!| findstr /c:" " >nul && echo !ip!| findstr /i /v "Windows" >nul && echo !ip!
+  )
+)
+endlocal
+echo.
+echo [++Unattend / Sysprep Credential Files]
+echo.
+for %%f in ("%SystemRoot%\Panther\Unattend.xml" "%SystemRoot%\Panther\Unattend\Unattended.xml" "%SystemRoot%\Panther\Unattended.xml" "%SystemRoot%\System32\Sysprep\sysprep.xml" "%SystemRoot%\System32\Sysprep\sysprep.inf" "%SystemDrive%\unattend.xml") do if exist %%f (
+  echo --- %%f ---
+  type %%f 2>nul | findstr /i "password username useraccount autologon"
+)
+echo.
+echo [++Writable Directories in PATH (DLL hijack surface)]
+echo.
+setlocal enabledelayedexpansion
+for %%d in ("%PATH:;=" "%") do (
+  set "dir=%%~d"
+  if exist "!dir!\" (
+    icacls "!dir!" 2>nul | findstr /i "Users:( Everyone:( \Users:(" | findstr /i "(F) (M) (W)" >nul && echo POTENTIALLY WRITABLE: !dir!
+  )
+)
+endlocal
 echo.
 del systeminfo.txt
 del hotfix.txt
